3c8b9f3d847dcac548cc647799f82f4a2af54e2f
MohamedAbdel-Azeem <mohamedk.abdelazeem@gmail.com>
2023-11-07 13:33:15 +0200

    Calculator Layout Done. Missing Buttons Functionality and Buttons Styling and Evaluating Expressions.

--- script.js
+++ script.js
@@ -14,6 +14,35 @@ function drawButtons(){
     row4.setAttribute('id','row4');
     row4.classList.add('buttonRows');
 
+
+    const delButton = document.createElement('button');
+    delButton.setAttribute('id','del');
+    delButton.textContent = 'DEL';
+    const clearButton = document.createElement('button');
+    clearButton.setAttribute('id','AC');
+    clearButton.textContent = 'AC';
+    row1.appendChild(clearButton);
+    row1.appendChild(delButton);
+
+    const addButton = document.createElement('button');
+    addButton.setAttribute('id','add');
+    addButton.textContent = '+';
+    const subButton = document.createElement('button');
+    subButton.setAttribute('id','sub');
+    subButton.textContent = '-';
+    row3.appendChild(subButton);
+    row3.appendChild(addButton);
+
+    const mulButton = document.createElement('button');
+    mulButton.setAttribute('id','mul');
+    mulButton.textContent = '*';
+    const divideButton = document.createElement('button');
+    divideButton.setAttribute('id','divide');
+    divideButton.textContent = '/';
+    row2.appendChild(divideButton);
+    row2.appendChild(mulButton);
+
+
     for (let i = 9; i >= 0 ; i--){
         const button = document.createElement('button');
         button.setAttribute('id',i);
@@ -25,9 +54,12 @@ function drawButtons(){
         } else if (i >= 1){
             row3.appendChild(button);
         } else {
-            row4.appendChild(button);
+            button.style.width = '230%'
+            row4.appendChild(button); // 0 button
         }
     }
+
+
 
     const equalButton = document.createElement('button');
     equalButton.setAttribute('id','equal');
@@ -35,6 +67,11 @@ function drawButtons(){
     const ansButton = document.createElement('button');
     ansButton.setAttribute('id','ans');
     ansButton.textContent = 'Ans';
+    const decButton = document.createElement('button');
+    ansButton.setAttribute('id','dec');
+    decButton.textContent = '.';
+    decButton.style.fontSize = '20px';
+    row4.appendChild(decButton);
     row4.appendChild(ansButton);
     row4.appendChild(equalButton);
 
@@ -51,6 +88,9 @@ function textAreaController(){
         const key = event.key;
         if (!regex.test(key)) {
           event.preventDefault();
+        }
+        if (key === 'Enter'){
+            console.log('Evaluate!'); // replace with an Evaluating Function
         }
       });
 
